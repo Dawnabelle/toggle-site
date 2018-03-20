940aa19e79cf4d6b8a17aa2266c2c5f5b9560c1a
Brenda Franco and Dawn Mott <brenda@>
2018-03-20 10:34:10 -0700

    add toggle functionality to different aspects of html

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,18 @@ $(document).ready(function() {
     $("#trail-showing").toggle();
     $("#trail-hidden").toggle();
   });
+});
 
+$(document).ready(function() {
+  $(".bridge-clickable").click(function() {
+    $("#bridge-showing").toggle();
+    $("#bridge-hidden").toggle();
+  });
+});
 
+$(document).ready(function() {
+  $(".coast-clickable").click(function() {
+    $("#coast-showing").slideToggle();
+    $("#coast-hidden").slideToggle();
+  });
 });
